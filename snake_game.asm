4ac52c8630b0e65ce3f47b218574b7aa67352c83
Hashim Zuraiqi <158300334+HashimZuraiqi@users.noreply.github.com>
2025-12-23 14:11:45 +0300

    Implement wall collision detection and game over handling

--- snake_game.asm
+++ snake_game.asm
@@ -2,6 +2,7 @@
     snake_length db 1   
     snake_x db 40 ,99 dup(?)
     snake_y db 12 ,99 dup(?)
+    game_msg db 'GAME OVER'
     direction db 0 ; iam implemnting the directions as values 0 for Right, 1 for down, 2 for left and 3 for up
 .stack
     dw 128 dup(?)
@@ -11,6 +12,7 @@
     mov ds, ax
     
     ;video text mode
+    video:
     mov ah, 0
     mov al, 03h
     int 10h
@@ -136,13 +138,7 @@
         je left_direction
         
         cmp al, 's'
-        je down_direction
-        
-        cmp al, 'd'
         je right_direction
-        
-        cmp al, 'w'
-        je up_direction
         
         jmp no_key
         
@@ -173,33 +169,31 @@
     jmp main_loop
     
     left_direction:
-        mov direction, 2
-        jmp no_key   
-        
-    right_direction:
-        mov direction, 0
-        jmp no_key   
-        
-    down_direction:
-        mov direction, 1 
-        jmp no_key
-        
-    up_direction:
+        dec direction
+        cmp direction, 0FFh
+        jne no_key
         mov direction, 3
         jmp no_key
         
+    right_direction:
+        inc direction
+        cmp direction, 4
+        jne no_key
+        mov direction, 0
+        jmp no_key   
+        
     right:
         add snake_x[si], 1
-        jmp draw_new
+        jmp check_wall_collision
     down:
         add snake_y[si], 1
-        jmp draw_new
+        jmp check_wall_collision
     left:
         sub snake_x[si], 1
-        jmp draw_new
+        jmp check_wall_collision
     up:
         sub snake_y[si], 1
-        jmp draw_new
+        jmp check_wall_collision
         
     draw_new: ;draw the head location
         mov ah, 02h
@@ -213,12 +207,61 @@
         int 10h
         
         jmp main_loop
-
+        
+    check_wall_collision:;checkss if the head is at 0 or 79 or y in 0 or 24 so it did hit the wall so the player lost
+        cmp snake_x[si], 0
+        je game_over
+        
+        cmp snake_x[si], 79 
+        je game_over
+        
+        cmp snake_y[si], 0
+        je game_over
+        
+        cmp snake_y[si], 24
+        je game_over
+        
+        jmp draw_new
+        
+    game_over:; if the player hit the wall he loses and then print game over message
+        mov ah, 02h
+        mov dh, 12
+        mov dl, 35
+        mov bh, 0
+        int 10h
+        
+        
+        mov ah, 0Eh
+        mov si, 0
+        mov cx, 9
+        print_msg:
+            mov al, game_msg[si]
+            int 10h
+            inc si
+        loop print_msg
+        
+    wait_restart:;after losing it waits until the user presses r to restart 
+        mov ah, 00h
+        int 16h
+        
+        cmp al, 'r'
+        je restart_game
+        jmp wait_restart
+        
+    restart_game:;restart the variables to its itial state before starting the game
+        mov snake_length, 1 
+        mov snake_x, 40
+        mov snake_y, 12
+        mov direction, 0
+        
+        ;now we need to reset the video mode to clear the screen and draw the border
+        jmp video
+        
 ;int to end the program
-mov ax, 4C00h
-int 21h
+;mov ax, 4C00h
+;int 21h
 
-
+                                                             
 
 
 
